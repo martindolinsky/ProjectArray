f53df5cd7b8697a7f825c0c081d1fddd92b4b0cd
Martin Dolinsky <martin.dolinsky@akademiasovy.sk>
2019-10-23 16:17:59 +0200

    Completed methods for today

--- src/sk/itsovy/dolinsky/array/Main.java
+++ src/sk/itsovy/dolinsky/array/Main.java
@@ -7,5 +7,9 @@ public class Main {
 
         MyArray.test1();
         MyArray.test2();
+        MyArray.test3();
+        MyArray.test4();
+        int[] results = MyArray.minmax(4, 2, 3);
+        System.out.println(results[0] + " " + results[1]);
     }
 }
--- src/sk/itsovy/dolinsky/array/MyArray.java
+++ src/sk/itsovy/dolinsky/array/MyArray.java
@@ -58,4 +58,129 @@ public class MyArray {
 		}
 	}
 
+	/**
+	 * Find arithmetic average number of array values
+	 * find minimum and maximum value of array
+	 * find second minimum and second maximum value of array
+	 * print how many even and how many odd numbers are in array
+	 */
+	public static void test3() {
+		System.out.println();
+
+		Random rn = new Random();
+		int[] arr = new int[12];
+
+		for (int i = 0; i <= 11; i++) {
+			arr[i] = rn.nextInt(90) + 10;
+			System.out.print(arr[i] + " ");
+		}
+
+		int sum = 0;
+		for (int i = 0; i < 12; i++) {
+			sum += arr[i];
+		}
+
+		double average = sum / 12.0;
+		System.out.println();
+		System.out.println("Average: " + average);
+
+		int min = arr[0];
+		int second = arr[0];
+		for (int i = 1; i < 12; i++) {
+			if (min > arr[i])
+				min = arr[i];
+		}
+
+		for (int i = 1; i < 11; i++) {
+			if (arr[i] < min) {
+				second = min;
+				min = arr[i];
+			} else if (arr[i] < second && arr[i] != min) {
+				second = arr[i];
+			}
+		}
+		System.out.println("SecondMinimum: " + second);
+		System.out.println("Minimum: " + min);
+
+		int max = arr[0];
+
+		for (int i = 1; i < 12; i++) {
+			if (max < arr[i]) {
+				max = arr[i];
+			}
+		}
+
+		for (int i = 1; i < 11; i++) {
+			if (arr[i] > max) {
+				second = max;
+				max = arr[i];
+			} else if (arr[i] > second && arr[i] != max) {
+				second = arr[i];
+			}
+		}
+
+		System.out.println("SecondMaximum: " + second);
+		System.out.println("Maximum: " + max);
+
+		int even = 0;
+		int odd = 0;
+		for (int i = 0; i < 12; i++) {
+			if (arr[i] % 2 == 0) {
+				even++;
+			} else {
+				odd++;
+			}
+		}
+		System.out.println("Even: " + even + " Odd: " + odd);
+	}
+
+	/**
+	 * Display fibonnaci sequence
+	 */
+	public static void test4() {
+		System.out.println();
+		int[] arr = new int[20];
+
+		arr[0] = 1;
+		arr[1] = 1;
+
+		for (int i = 2; i < 20; i++) {
+			arr[i] = arr[i - 1] + arr[i - 2];
+		}
+
+		for (int i = 0; i < 20; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+
+	public static int[] minmax(int a, int b, int c) {
+		System.out.println();
+
+		int[] result = new int[2];
+
+		if (a <= b && a <= c) {
+			result[0] = a;
+		}
+		if (b <= a && b <= c) {
+			result[0] = b;
+		}
+		if (c <= a && c <= b) {
+			result[0] = c;
+		}
+		if (a >= b && a >= c) {
+			result[1] = a;
+		}
+		if (b >= a && b >= c) {
+			result[1] = b;
+		}
+		if (c >= b && c >= a) {
+			result[1] = c;
+		}
+
+
+		return result;
+
+	}
+
 }
